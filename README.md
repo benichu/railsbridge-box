a36087c87b427a6a3d4b66cbb382d3a7f839ae41
Ben Thouret <ben@2ret.com>
2014-03-24 21:06:04 -0400

    Packer configuration to build a brand new Vbox image [in progress]
    TODO: extract the dev env build in a standard vagrant packaging
    (./scripts/setup.sh)

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 railsbridge-box
 ===============
 
-Packer files used for RailsbridgeMTL development workstations
+[Packer](http://www.packer.io/) files used for RailsbridgeMTL development workstations
+
+## Prerequisites
+
+- [Packer 1.5+](http://www.packer.io/)
+- [VirtualBox 4.3+](https://www.virtualbox.org/wiki/Downloads)
+- [iso/ubuntu-13.10-server-amd64.iso](http://www.ubuntu.com/download/server)
+- [Vagrant 1.3.5+](http://downloads.vagrantup.com/)
+
+## Building a brand new virtual box
+
+Customize your environment by editing some of the parameters in those files:
+
+- Box settings: `./packer.json`
+- Development environment: `./scripts/setup.sh`
+
+Start building your virtual machine:
+
+```bash
+$ ./create_box
+```
+
+Wait (quite a long time...) and you can test your brand new vagrant box:
+
+```bash
+$ cd railsbridge-box
+$ vagrant destroy
+$ vagrant up
+$ vagrant ssh
+```
+
+You can then distribute your vagrant box build: `railsbridge-iso_virtualbox.box`
+
+## Vagrantfile
+
+```ruby
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "railsbridge_mtl_201304"
+  config.vm.box_url = "http://.../railsbridge-iso_virtualbox.box"
+  config.vm.box_download_checksum = "..."
+  config.vm.box_download_checksum_type = "" # supported "md5", "sha1", and "sha256".
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+end
+```
+
+## License
+
+See ./LICENSE
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,19 +4,20 @@ set -e
 
 echo "Installing for Railsbridge-MTL"
 
-# Install vagrant keys
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-cd ~/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-chown -R vagrant ~/.ssh
+# Required Libs
+apt-get -y -q install curl wget git-core vim
+apt-get -y -q install build-essential openssl \
+                      gawk libreadline6-dev zlib1g zlib1g-dev \
+                      libssl-dev libyaml-dev libsqlite3-dev \
+                      sqlite3 autoconf libgdbm-dev libncurses5-dev \
+                      automake libtool bison pkg-config libffi-dev \
+                      libxml2-dev libxslt-dev libxml2 libmagick++-dev
 
 # Railsbridge MOTD
-sudo apt-get install update-motd
-sudo rm /etc/update-motd.d/*
+apt-get -y -q install update-motd
+rm /etc/update-motd.d/*
 
-sudo sh -c "cat > /etc/update-motd.d/10-railsbridge << 'EOT'
+sh -c "cat > /etc/update-motd.d/10-railsbridge << 'EOT'
 #!/bin/sh
 
 echo '   ========================================'
@@ -31,26 +32,51 @@ echo '   | |_) ||  _ <  | | | |_| || |_| || |___'
 echo '   |____/ |_| \_\|___||____/  \____||_____|'
 echo ''
 echo '   ========================================'
+echo ''
+echo '   Welcome to the Railsbridge Montreal virtual machine!  The RailsBridge VM is a *computer within your computer*'
+echo '   running the Linux operating system.  Everything you need is installed, including:'
+echo '   Ruby 2.1, Rails 4.0, sqlite3, the heroku toolbelt, and git.'
+echo ''
+echo '   When you start your Rails app, visit http://localhost:3000 in your web browser.
+echo ''
+echo '   The ~/workspace directory is shared with the folder on your laptop where you created this VM.  Any file you'
+echo '   put there in your laptop will appear here in ~/workspace.  This lets you edit files with an OSX or Windows'
+echo '   text editor, and run them here in the VM.'
+echo ''
+echo '   To leave the virtual machine, type'
+echo '       exit'
+echo '   and hit return. '
+echo ''
+echo '   ========================================'
 
 EOT"
 
-sudo chmod +x /etc/update-motd.d/10-railsbridge
+chmod +x /etc/update-motd.d/10-railsbridge
 # Force message
-sudo run-parts /etc/update-motd.d/
+run-parts /etc/update-motd.d/
 
 # Install rbenv and Ruby
 rm -rf /home/vagrant/.rbenv
 git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
-echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bash_profile
-source /home/vagrant/.bash_profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
+echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
 git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
-rbenv install 2.1.1 && rbenv global 2.1.1
+
+chown -R vagrant:vagrant /home/vagrant
+
+source /home/vagrant/.bashrc || true
+/home/vagrant/.rbenv/bin/rbenv install 2.1.1
+/home/vagrant/.rbenv/bin/rbenv rehash
+/home/vagrant/.rbenv/bin/rbenv global 2.1.1
 
 # get Rails running
-cd /vagrant
 echo "gem: --no-ri --no-rdoc" > /home/vagrant/.gemrc
-gem install bundler
-gem install rails
+gem install bundler || true
+gem install rails || true
 
-#
+# Heroku Toolbelt is a .deb package, so install as root.
+# curl -L https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Our bash setup will cd to workspace on login.
+# cd /home/vagrant
+# ln -s /vagrant workspace
